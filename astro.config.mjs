4f843e949d96a5243bd0847ba5f68fd964b66de1
khaitbek <khaitbek2005@gmail.com>
2025-12-21 11:36:29 +0500

    fix posthog

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,10 @@ export default defineConfig({
 				access: "public",
 				context: "client",
 			}),
+			NODE_ENV: envField.string({
+				access: "public",
+				context: "client",
+			}),
 		},
 	},
 });
