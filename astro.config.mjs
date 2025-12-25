b0896de1834f704d6b42d4ed1a180650ef2e1602
khaitbek <khaitbek2005@gmail.com>
2025-12-25 23:29:13 +0500

    add compressor options

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,31 +4,33 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, envField } from "astro/config";
 
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-	integrations: [react()],
-	env: {
-		schema: {
-			APP_URL: envField.string({
-				access: "public",
-				context: "client",
-			}),
-			POSTHOG_PROJECT_ID: envField.string({
-				access: "public",
-				context: "client",
-			}),
-			NODE_ENV: envField.string({
-				access: "public",
-				context: "client",
-			}),
-		},
-	},
-	i18n: {
-		locales: ["en", "uz", "ru"],
-		defaultLocale: "en",
-	},
+    integrations: [react(), playformCompress()],
+    env: {
+        schema: {
+            APP_URL: envField.string({
+                access: "public",
+                context: "client",
+            }),
+            POSTHOG_PROJECT_ID: envField.string({
+                access: "public",
+                context: "client",
+            }),
+            NODE_ENV: envField.string({
+                access: "public",
+                context: "client",
+            }),
+        },
+    },
+    i18n: {
+        locales: ["en", "uz", "ru"],
+        defaultLocale: "en",
+    },
 });
