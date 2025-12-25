95a2a24d81daff54adb9eaabe91aa2d691539b78
khaitbek <khaitbek2005@gmail.com>
2025-12-25 23:07:24 +0500

    use webp and svg for local images

--- src/widgets/logo/widget.tsx
+++ src/widgets/logo/widget.tsx
@@ -1,7 +1,7 @@
 export function AppLogo() {
 	return (
 		<img
-			src={"/images/blue_logo.png"}
+			src={"/images/blue_logo.svg"}
 			alt="SkySearch"
 			className="rounded-lg overflow-hidden"
 			width={50}
